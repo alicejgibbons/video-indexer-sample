98639199edd1a1db30580709492143f2ff8729e8
alexandre brisebois <brisebois@outlook.com>
2018-07-06 13:41:48 -0400

    permissions are working working

--- samples/MediaSample/MediaSample.Droid/MainActivity.cs
+++ samples/MediaSample/MediaSample.Droid/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Plugin.CurrentActivity;
 using Plugin.Permissions;
 using Plugin.Permissions.Abstractions;
 
@@ -18,14 +19,16 @@ namespace MediaSample.Droid
         {
             base.OnCreate(bundle);
 
-            global::Xamarin.Forms.Forms.Init(this, bundle);
+	        CrossCurrentActivity.Current.Init(this, bundle);
+
+			global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
         {
-            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-    }
+			PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+		}
+	}
 }
 
